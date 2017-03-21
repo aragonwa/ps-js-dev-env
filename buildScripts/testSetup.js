ecddfcb07ef74800a9cba19517b296f418726001
Alex Aragon <alexaragon@ACA8006B.ipt.aol.com>
2017-03-20 20:06:00 -0700

    Adding testing framework and assertion library

---
+++ buildScripts/testSetup.js
@@ -0,0 +1,9 @@
+// This file isn't tranpiled, so must use CommonJS and ES5
+
+// Register Babel to transpile before our tests run
+require('babel-register')();
+
+// Disable webpack features that Mocha doesn't understand
+require.extensions['.css'] = function(){};
+
+
